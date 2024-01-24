2beaf93807b7228e557abe17308579a1fec04f09
qurs3l <qureshiseline@gmail.com>
2024-01-24 14:23:09 -0600

    added delete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: %i[ show edit update destroy ]
+  before_action :set_book, only: %i[ show edit update destroy delete]
 
   # GET /books or /books.json
   def index
@@ -17,6 +17,10 @@ class BooksController < ApplicationController
 
   # GET /books/1/edit
   def edit
+  end
+
+  #GET /books/1/delete
+  def delete
   end
 
   # POST /books or /books.json
@@ -47,7 +51,7 @@ class BooksController < ApplicationController
     end
   end
 
-  # DELETE /books/1 or /books/1.json
+  # DELETE /books/1/delete or /books/1.json
   def destroy
     @book.destroy
 
